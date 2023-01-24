1c9c3526233a6c20fe4d6ebb406bc2058787fa6f
IsaacStyle <isaac.opra@gmail.com>
2023-01-24 05:55:55 -0500

    Atlas seed

--- server/connection.js
+++ server/connection.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 let MONGODB_URI =
   process.env.MONGO_URL ||
   'mongodb://127.0.0.1:27017/Restaurant-Api'
+  // 'mongodb+srv://IsaacO:Iamstan12@restaurant-api.1xmyrti.mongodb.net/Restaurant-Api'
 
   let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true}
 
@@ -13,6 +14,7 @@ mongoose
   .catch((error) => console.error('Error connecting to MongoDB: ', error.message))
 
 
+mongoose.connection.on('connected', () => console.log(`Connected to MongoDB!`))
 mongoose.connection.on('disconnected', () => console.log(`Disconnected from MongoDB!`))
 
 
