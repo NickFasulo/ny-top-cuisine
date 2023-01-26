bc1f9957c0340e09da274aa048adf0ec8b3eaede
orpichardo <orlandopichardo809@gmail.com>
2023-01-26 14:09:11 -0500

    Trying to make the slide show work in the home page

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -4,6 +4,81 @@ import { getRestaurants } from "../services/restaurants.js";
 import { useEffect, useState } from "react";
 
 
+// export default function Home() {
+//   const [home, setHome] = useState([]);
+
+//   useEffect(() => {
+//     fetchRestaurants();
+//   }, []);
+
+//   async function fetchRestaurants() {
+//     const allRestaurants = await getRestaurants();
+//     setHome(allRestaurants);
+//   };
+
+
+
+//   return (
+//     <div>
+//       <h1>New York Top Cuisine</h1>
+//       <div className="rest-container">
+//         {home.map((restData) => (
+//           <>
+//             <img className="image" src={restData.logo_photos} />
+//             <h1>{restData.name}</h1>
+//             <h3>Rating: {restData.weighted_rating_value}</h3>
+//             <h4>City: {restData.address.city}, {restData.address.state}</h4>
+
+//           </>
+//         ))
+//         }
+//       </div>
+//     </div>
+//   )
+// }
+
+
+
+
+// // export default function Home() {
+//   const [data, setData] = useState({});
+// const [restaurantIndex, setRestaurantIndex] = useState(1)
+
+// useEffect(() => {
+// fetch(`https://nytc.herokuapp.com${restaurantsIndex}`)
+// .then(res => res.json())
+// .then(data => setData(data))
+// }, [restaurantIndex]);
+
+// function nextRestaurants()
+// {
+//     setPokemonIndex((prev) => prev ++)
+
+// }
+
+// function prevRestaurant()
+// {
+//     setRestaurantIndex(((prev) => prev - 1))
+
+// }
+//   // return (
+    
+//    < div className="box">
+
+//     <button onClick={() => prevRestaurant()} >prev</button>
+
+//     <Card data={data}/>
+
+//     <button onClick={() => {
+//       setRestaurantIndex(((prev) => prev + 1))
+//     }}>next</button>
+
+//    </div>
+//   );
+// }
+
+
+
 export default function Home() {
   const [home, setHome] = useState([]);
 
@@ -15,6 +90,34 @@ export default function Home() {
     const allRestaurants = await getRestaurants();
     setHome(allRestaurants);
   };
+
+
+  function nextRestaurants() {
+    setRestaurants((prev) => prev++)
+
+  }
+
+  function prevRestaurants() {
+    setRestaurants(((prev) => prev - 1))
+
+  }
+  return (
+
+
+    
+    < div className="box">
+
+      <button onClick={() => prevRestaurants()} >prev</button>
+
+      <Restaurant data={data} />
+
+      <button onClick={() => {
+        setRestaurants(((prev) => prev + 1))
+      }}>next</button>
+
+    </div>
+  );
+
 
 
 
@@ -34,8 +137,90 @@ export default function Home() {
         }
       </div>
     </div>
-  )
+  );
 }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   const [data, setData] = useState({});
+// const [pokemonIndex, setPokemonIndex] = useState(1)
+
+// useEffect(() => {
+// fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`)
+// .then(res => res.json())
+// .then(data => setData(data))
+// }, [pokemonIndex]);
+
+// function nextPokemon()
+// {
+//     setPokemonIndex((prev) => prev ++)
+
+// }
+
+// function prevPokemon()
+// {
+//     setPokemonIndex(((prev) => prev - 1))
+
+// }
+//   return (
+//    < div className="box">
+
+//     <button onClick={() => prevPokemon()} >prev</button>
+
+//     <Card data={data}/>
+
+//     <button onClick={() => {
+//       setPokemonIndex(((prev) => prev + 1))
+//     }}>next</button>
+
+//    </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
