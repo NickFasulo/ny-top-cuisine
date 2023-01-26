eabbeb02b61e2bd16be2d592848c77caaff1820a
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-26 13:04:51 -0500

    add star rating

--- client/src/components/Restaurant/Restaurant.jsx
+++ client/src/components/Restaurant/Restaurant.jsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom'
 import './Restaurant.css'
 
 export default function Restaurant({ restaurant }) {
+  console.log(restaurant)
+
   return (
     <div className='rest-card'>
       <Link to={`/${restaurant._id}`}>
-        <img src={restaurant.food_photos[0] || restaurant.logo_photos[0]} />
+        <img src={restaurant.logo_photos[0] || 'placeholder.jpg'} />
+        <span>⭐ {Math.round(restaurant.weighted_rating_value * 10) / 10}</span>
         <div className='rest-card-body'>
           <h3>{restaurant.name}</h3>
         </div>
